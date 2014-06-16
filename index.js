2eee59c2c0fb74a750949942ef0d6a843e8a2590
simokhalil <khalil_simo@hotmail.fr>
2014-06-16 16:08:07 +0200

    Object rotation

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
+var rotWorldMatrix;
+var rotObjectMatrix;
 
 init();
 animate();
@@ -59,7 +61,14 @@ function init() {
     // model
 
     var loader = new THREE.OBJMTLLoader();
-    loader.load( 'examples/cow.obj', 'examples/cow.obj.mtl', function ( object ) {
+    //loader.load( 'examples/rivergod/mesh.obj', 'examples/rivergod/mesh.mtl', function ( object ) {
+    //loader.load( 'examples/cow.obj', 'examples/cow.obj.mtl', function ( object ) {
+    loader.load( 'examples/maya/mesh.obj', 'examples/maya/mesh.mtl', function ( object ) {
+
+        var zAxis = new THREE.Vector3(1,0,0);
+        var xAxis = new THREE.Vector3(0,1,0);
+        object.rotateOnAxis(zAxis, 90 * Math.PI/180);
+        object.rotateOnAxis(xAxis, -90 * Math.PI/180);
 
         scene.add( object );
 
@@ -96,9 +105,15 @@ function onDocumentMouseMove( event ) {
 
 function animate() {
 
-    requestAnimationFrame( animate );
-    render();
 
+
+    requestAnimationFrame( animate );
+
+    //var xAxis = new THREE.Vector3(1,0,0);
+    //var zAxis = new THREE.Vector3(0,0,1);
+    //rotateAroundWorldAxis(object, zAxis, Math.PI / 90);
+
+    render();
 }
 
 function render() {
@@ -106,4 +121,41 @@ function render() {
     controls.update(); //for cameras
     renderer.render( scene, camera );
 
+}
+
+
+function rotateAroundObjectAxis(object, axis, radians) {
+    rotObjectMatrix = new THREE.Matrix4();
+    rotObjectMatrix.makeRotationAxis(axis.normalize(), radians);
+
+    // old code for Three.JS pre r54:
+    // object.matrix.multiplySelf(rotObjectMatrix);      // post-multiply
+    // new code for Three.JS r55+:
+    object.matrix.multiply(rotObjectMatrix);
+
+    // old code for Three.js pre r49:
+    // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
+    // new code for Three.js r50+:
+    object.rotation.setEulerFromRotationMatrix(object.matrix);
+}
+
+
+// Rotate an object around an arbitrary axis in world space
+function rotateAroundWorldAxis(object, axis, radians) {
+    rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+
+    // old code for Three.JS pre r54:
+    //  rotWorldMatrix.multiply(object.matrix);
+    // new code for Three.JS r55+:
+    rotWorldMatrix.multiply(object.matrix);                // pre-multiply
+
+    object.matrix = rotWorldMatrix;
+
+    // old code for Three.js pre r49:
+    // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
+    // old code for Three.js pre r59:
+    // object.rotation.setEulerFromRotationMatrix(object.matrix);
+    // code for r59+:
+    object.rotation.setFromRotationMatrix(object.matrix);
 }
