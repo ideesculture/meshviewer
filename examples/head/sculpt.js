abc1745a45df04214047bfcabcc9640e9d64dfb7
GautierMichelin <gm@ideesculture.com>
2014-08-07 14:20:34 +0200

    Adding UTF8 & objzip versions

---
+++ examples/head/sculpt.js
@@ -0,0 +1,19 @@
+{
+  "materials": {
+    "texture_0": { "map_Kd": "tex_0.jpg" },
+    "untextured": { "Kd": [0, 0, 0] },
+    "no_material": { "Kd": [0, 0, 0] }
+  },
+  "decodeParams": {
+    "decodeOffsets": [-8035,-3848,-15064,0,0,-511,-511,-511],
+    "decodeScales": [0.001898,0.001898,0.001898,0.000978,0.000978,0.001957,0.001957,0.001957]
+  },
+  "urls": {
+    "sculpt.utf8": [
+      { "material": "texture_0",
+        "attribRange": [0, 25693],
+        "codeRange": [205544, 97324, 48534]
+      },
+    ]
+  }
+}
